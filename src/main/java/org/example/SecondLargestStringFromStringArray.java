ad7403859c876695553a019916934a979f173803
Ravi Saini <thisisravisaini@gmail.com>
2025-10-27 21:21:25 +0530

    Program

---
+++ src/main/java/org/example/SecondLargestStringFromStringArray.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SecondLargestStringFromStringArray {
+
+
+    public static void secondLargestString(String arr[]) {
+
+        TreeMap<Integer, String> tm = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            tm.put(arr[i].length(), arr[i]);
+        }
+
+        System.out.println(tm);
+
+
+        System.out.println(tm.lastEntry().getKey());
+        System.out.println(tm.lowerEntry(tm.lastEntry().getKey()));
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String strArr[] = {"RaviSaini", "Taruna", "Papa", "Maa"};
+        secondLargestString(strArr);
+
+    }
+
+}
