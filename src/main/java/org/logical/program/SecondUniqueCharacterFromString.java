0d56b6e5bd78501f8624772b6ddcb4ade0b80426
Ravi Saini <ThisisRaviSaini@gmail.com>
2025-11-03 14:15:36 +0530

    Interview Program

---
+++ src/main/java/org/logical/program/SecondUniqueCharacterFromString.java
@@ -0,0 +1,50 @@
+package org.logical.program;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SecondUniqueCharacterFromString {
+
+// Input    aakkaklammz
+// Output    Second unique letter
+//           z
+
+
+    public static void findSecondUniqueLetter(String str, int pos) {
+
+        char[] charArr = str.toCharArray();
+        int index = pos - 1;
+
+        ArrayList<Character> al = new ArrayList<>();
+
+        LinkedHashMap<Character, Integer> lhm = new LinkedHashMap<>();
+
+        for (int i = 0; i < charArr.length; i++) {
+            al.add(charArr[i]);
+        }
+
+        for (int i = 0; i < charArr.length; i++) {
+            lhm.put(charArr[i], Collections.frequency(al, charArr[i]));
+        }
+
+        ArrayList<Character> uniqueElementAl = new ArrayList<>();
+
+        for (Map.Entry<Character, Integer> l : lhm.entrySet()) {
+            if (l.getValue() == 1) {
+                uniqueElementAl.add(l.getKey());
+            }
+        }
+
+        System.out.println("Second Unique Character: " + uniqueElementAl.get(index));
+
+    }
+
+    public static void main(String[] args) {
+        String str = "aakkaklammz";
+        findSecondUniqueLetter(str, 2);
+
+    }
+}
+
---
+++ src/main/java/org/logical/program/ThirdLargestNumberFromArray.java
@@ -0,0 +1,35 @@
+package org.logical.program;
+
+import java.util.TreeSet;
+
+public class ThirdLargestNumberFromArray {
+
+
+    //input: {5,4,6,3,8,6,5,9}
+    //output:  Third Largest Number:  6
+
+    public static void thirdLargestNumberFromArray(int[] arr, int whichLargestNumber) {
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            ts.add(arr[i]);
+        }
+        Integer last = ts.last();
+        for (int i = 1; i < whichLargestNumber; i++) {
+            last = ts.lower(last);
+        }
+        System.out.println("Third last: " + last);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 4, 6, 3, 8, 6, 5, 9, 10};
+        int whichLargestNumber = 3;
+
+        thirdLargestNumberFromArray(arr, whichLargestNumber);
+    }
+}
+
+
+
+
