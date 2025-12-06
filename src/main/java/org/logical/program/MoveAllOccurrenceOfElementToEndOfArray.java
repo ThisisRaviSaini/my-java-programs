70cb2d1359527697af9d5377b8a24d2bd1641999
Ravi Saini <ThisisRaviSaini@gmail.com>
2025-12-06 15:30:48 +0530

    New Program

--- src/main/java/org/logical/program/MoveAllOccurrenceOfElementToEndOfArray.java
+++ src/main/java/org/logical/program/MoveAllOccurrenceOfElementToEndOfArray.java
@@ -6,7 +6,7 @@ public class MoveAllOccurrenceOfElementToEndOfArray {
 
     public static void moveToEnd(int[] arr, int num) {
         int pointer = 0;
-        
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != num) {
                 arr[pointer] = arr[i];
---
+++ src/main/java/org/logical/program/RemoveTheSubStringFromString.java
@@ -0,0 +1,24 @@
+package org.logical.program;
+
+public class RemoveTheSubStringFromString {
+
+
+    //Given two strings, base and remove, return a version of the base string
+    // where all instances of the remove string have been removed (not case sensitive).
+    //You may assume that the remove string is length 1 or more.
+    //Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+    //
+    //Ex: base= "Hello there" & remove="llo"
+    //
+    //Below are the sample test cases with expected output:
+    //
+    //withoutString("Hello there", "llo") → "He there"
+    //withoutString("MkjtMkx", "Mk") → "jtx"
+    //withoutString("THIS is a FISH", "is") → "TH a FH"
+    //withoutString("THIS is a FISH", "iS") → "TH a FH"
+    //withoutString("abxxxab", "xx") → "abxab"
+    
+    public static void main(String[] args) {
+
+    }
+}
