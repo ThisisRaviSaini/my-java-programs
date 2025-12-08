18d77569f5060679e290a1fbe347eaa29567805b
Ravi Saini <ThisisRaviSaini@gmail.com>
2025-12-08 17:27:05 +0530

    Reverse The Word Only  In Sentence

---
+++ src/main/java/org/logical/program/ReverseTheSingleWordOnlyInSentence.java
@@ -0,0 +1,28 @@
+package org.logical.program;
+
+public class ReverseTheSingleWordOnlyInSentence {
+
+    public static void reverseTheWord(String str, String wordTobeReversed) {
+
+        String[] strArr = str.split(" ");
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].contains(wordTobeReversed)) {
+                System.out.print(reverseTheWord(strArr[i]) + " ");
+            } else {
+                System.out.print(strArr[i] + " ");
+            }
+        }
+    }
+
+    public static StringBuffer reverseTheWord(String word) {
+        StringBuffer sb = new StringBuffer(word);
+        return sb.reverse();
+    }
+
+
+    public static void main(String[] args) {
+        String str = "This word is an example";
+        String wordTobeReversed = "word";
+        reverseTheWord(str, wordTobeReversed);
+    }
+}
